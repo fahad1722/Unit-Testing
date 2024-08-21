e92a58304df94158efe7a7ea387bb9d902dddda3
Fahad <mohammedfahad1905@gmail.com>
2024-08-21 21:51:14 +0530

    Updated controller

--- Unit-Testing/src/main/java/com/test/controllers/UserController.java
+++ Unit-Testing/src/main/java/com/test/controllers/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@PostMappin
+	@PostMapping
 	public ResponseEntity<User> createUser(@RequestBody User user) {
 		User createdUser = userService.createUser(user);
 		return new ResponseEntity<>(createdUser, HttpStatus.OK);
